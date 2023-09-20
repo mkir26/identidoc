8d1b6d7dc4f00325e96d9320c966e6709c65ecf1
mkir26 <558620@mail.ru>
2023-09-20 22:57:05 +0300

    erroe deploy

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ set -e
 echo "Starting identidock system"
 
 docker run -d --restart=always --name redis redis
-docker run -d --restart=always --name alex1255/dnmonster:1.0
+docker run -d --restart=always --name dnmonster alex1255/dnmonster:1.0
 docker run -d --restart=always --link dnmonster:dnmonster --link redis:redis -e ENV=PROD --name identidock alex1255/identidock:1.0
 docker run -d --restart=always --name proxy --link identidock:identidock -p 80:80 -e NGINX_HOST=10.0.1.6 -e NGINX_PROXY=http://identidock:9090 alex1255/proxy:1.0
 echo "Started"
